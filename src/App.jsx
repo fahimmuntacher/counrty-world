0e704ac4ceec0d9dfed5f20819db21b63a0c9cc4
fahimmuntacher <muntacherfahim234@gmail.com>
2025-09-18 19:35:07 +0600

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './Components/countries/countries'
+function App() {
+ 
+ return (
+    <>
+      <h1>Hello kacher bhia</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/countries/Countries.jsx
@@ -0,0 +1,10 @@
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hello from countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
